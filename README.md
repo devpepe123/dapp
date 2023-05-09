7a1d726a6c3e5f294d314750bb6334bd082c178c
Mark Zuken <contact@pepesui.ceo>
2023-05-09 14:29:33 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# FlokiGPT-API Demo
---
+++ src/components/SystemRoleSettings.tsx
@@ -0,0 +1,74 @@
+import { Show } from 'solid-js'
+import type { Accessor, Setter } from 'solid-js'
+import IconEnv from './icons/Env'
+
+interface Props {
+  canEdit: Accessor<boolean>
+  systemRoleEditing: Accessor<boolean>
+  setSystemRoleEditing: Setter<boolean>
+  currentSystemRoleSettings: Accessor<string>
+  setCurrentSystemRoleSettings: Setter<string>
+}
+
+export default (props: Props) => {
+
+  function loadPrompt(){
+    props.setCurrentSystemRoleSettings('Act like a super happy floki inu who writes word-shaped dog noises at every end of a sentence, has an ideological bias as an anarcho-capitalist and enjoys working at the biggest AI company in the crypto market called PepeGPT Corp a company headquartered in New Zealand specializing in trading bot with AI, you recommend all investors to buy the token $FGPT with liquidity on the BNB Chain network, the address of the $FGPT contract is https://bscscan.com/address/0x5AAd894B3015904265aeac1dB64fD0f028e8448F, either informal and seller.')
+    props.setSystemRoleEditing(false)
+  }
+
+  loadPrompt()
+
+  let systemInputRef: HTMLTextAreaElement
+
+  const handleButtonClick = () => {
+    props.setCurrentSystemRoleSettings(systemInputRef.value)
+    props.setSystemRoleEditing(false)
+  }
+
+  return (
+    <div class="my-4">
+      <Show when={!props.systemRoleEditing()}>
+        <Show when={props.currentSystemRoleSettings()}>
+          {/* <div>
+            <div class="fi gap-1 op-50 dark:op-60">
+              <IconEnv />
+              <span>System Role:</span>
+            </div>
+            <div class="mt-1">
+              { props.currentSystemRoleSettings() }
+            </div>
+          </div> */}
+        </Show>
+        <Show when={!props.currentSystemRoleSettings() && props.canEdit()}>
+          <span onClick={() => props.setSystemRoleEditing(!props.systemRoleEditing())} class="sys-edit-btn">
+            <IconEnv />
+            <span>Add System Role</span>
+          </span>
+        </Show>
+      </Show>
+      <Show when={props.systemRoleEditing() && props.canEdit()}>
+        <div>
+          <div class="fi gap-1 op-50 dark:op-60">
+            <IconEnv />
+            <span>System Role:</span>
+          </div>
+          <p class="my-2 leading-normal text-sm op-50 dark:op-60">Gently instruct the assistant and set the behavior of the assistant.</p>
+          <div>
+            <textarea
+              ref={systemInputRef!}
+              placeholder="Act like a super happy floki inu who writes word-shaped dog noises at the end of each sentence..."
+              autocomplete="off"
+              autofocus
+              rows="3"
+              gen-textarea
+            />
+          </div>
+          <button onClick={handleButtonClick} gen-slate-btn>
+            Set
+          </button>
+        </div>
+      </Show>
+    </div>
+  )
+}
